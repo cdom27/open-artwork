caad2914e20c1fa47ad46cd844db7cc404b26252
Cristian Dominguez <c.isaacdominguez@gmail.com>
2026-09-22 19:32:57 -0700

    placeholder gallery page

---
+++ apps/web/src/routes/gallery/+page.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+	<title>Gallery - Open Artwork</title>
+	<meta
+		name="description"
+		content="Open Artwork is an open art data project for exploring artists, artworks, metadata, images, and color palettes. It is primarily a portfolio and learning project for exploring frontend development, backend architecture, and image-processing work."
+	/>
+</svelte:head>
+
+<section class="flex h-screen flex-col gap-8 p-4">
+	<h1 class="text-lg">Gallery Page</h1>
+</section>
