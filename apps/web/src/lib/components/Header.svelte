f243947aee48c23d00cd686c5d27e9bad6d168be
Cristian Dominguez <c.isaacdominguez@gmail.com>
2026-09-23 21:30:26 -0700

    animate mobile menu in/out

--- apps/web/src/lib/components/Header.svelte
+++ apps/web/src/lib/components/Header.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
+	import { fly } from 'svelte/transition';
 	import { headerLinks } from '$lib/data/nav-links';
-	import { inView } from '$lib/actions/in-view';
 	import Button from './Button.svelte';
 	import Link from './Link.svelte';
 
-	let open = $state(true);
+	let open = $state(false);
 </script>
 
-<header class="group relative p-2 sm:px-10 sm:py-5 md:px-15">
+<header class="relative p-2 sm:px-10 sm:py-5 md:px-15">
 	<div class="flex items-center justify-between">
 		<a href="/" onclick={() => (open = false)}>Open Artwork</a>
 
@@ -21,27 +21,35 @@
 
 		<nav class="hidden md:flex">
 			<ul class="flex gap-5.5">
-				{#each headerLinks as linkGroup}
-					<Link variant="tertiary" href={linkGroup.href} onclick={() => (open = false)}
-						>{linkGroup.name}</Link
+				{#each headerLinks as link}
+					<Link variant="tertiary" href={link.href} onclick={() => (open = false)}>{link.name}</Link
 					>
 				{/each}
 			</ul>
 		</nav>
 	</div>
 
-	<nav class={`absolute inset-x-2 mt-5.5 sm:inset-x-10 md:inset-x-15 ${open ? 'flex' : 'hidden'}`}>
-		<ul class=" ml-auto flex flex-col gap-5.5">
-			{#each headerLinks as linkGroup}
-				<li class="flex self-end">
-					<Link
-						variant="tertiary"
-						cn="self-end"
-						href={linkGroup.href}
-						onclick={() => (open = false)}>{linkGroup.name}</Link
+	{#if open}
+		<nav
+			class="absolute inset-x-2 mt-5.5 flex sm:inset-x-10 md:inset-x-15"
+			transition:fly={{ y: -10, duration: 300 }}
+		>
+			<ul class="ml-auto flex flex-col gap-5.5">
+				{#each headerLinks as linkGroup, i}
+					<li
+						class="flex self-end"
+						in:fly|global={{ y: -10, duration: 300, delay: i * 80 }}
+						out:fly|global={{ y: -10, duration: 200, delay: (headerLinks.length - 1 - i) * 60 }}
 					>
-				</li>
-			{/each}
-		</ul>
-	</nav>
+						<Link
+							variant="tertiary"
+							cn="self-end"
+							href={linkGroup.href}
+							onclick={() => (open = false)}>{linkGroup.name}</Link
+						>
+					</li>
+				{/each}
+			</ul>
+		</nav>
+	{/if}
 </header>
